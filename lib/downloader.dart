4261ede5003cffdfed7f55e4191288e67f52975f
changlei <changlei@graspyun.com>
2021-07-29 18:40:57 +0800

    feat: 初始提交

--- lib/downloader.dart
+++ lib/downloader.dart
@@ -1,4 +1,6 @@
 library downloader;
 
+export 'package:downloader/src/downloader.dart';
 export 'package:downloader/src/m3u8.dart' hide decrypt;
 export 'package:downloader/src/m3u8_downloader.dart';
+export 'package:downloader/src/universal_downloader.dart';
