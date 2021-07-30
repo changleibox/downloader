8710cad9a0b8b1b35ed719b00d637fd3dd0d6d6a
changlei <changlei@graspyun.com>
2021-07-30 20:14:54 +0800

    feat: 初始提交

--- lib/src/dio/downloader_dio.dart
+++ lib/src/dio/downloader_dio.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 import 'dart:collection';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 
 import 'entry_stub.dart'
 // ignore: uri_does_not_exist
@@ -24,35 +23,39 @@ class DownloaderDio with DioMixin implements Dio {
 
 const _capacity = 2;
 
-// 正在等待的队列
-final _waitingQueue = ListQueue<Future>(_capacity);
-
-// 正在进行的队列
-final _ongoingQueue = ListQueue<Future>(_capacity);
-
 /// 扩展dio
 /// 为了防止一次性请求太多，我们限制最多只能进行[_capacity]个请求
 mixin DownloaderDioMixin on DioMixin {
+  // 正在等待的队列
+  final _waitingQueue = ListQueue<Function>(_capacity);
+
+  // 正在进行的队列
+  final _ongoingQueue = ListQueue<Function>(_capacity);
+
   @override
   Future<Response<T>> fetch<T>(RequestOptions requestOptions) async {
-    final future = super.fetch<T>(requestOptions);
-    // 当正在进行的请求达到上限，则把新进来的请求放在等待队列中，等待进行中队列请求完成
-    if (_ongoingQueue.length < _capacity && _waitingQueue.isEmpty) {
-      _ongoingQueue.add(future);
-    } else {
-      _waitingQueue.add(future);
-    }
-    await compute(_fetch, future);
-    return future.whenComplete(() {
+    final completer = Completer<Response<T>>();
+    void next() {
       _ongoingQueue.removeFirst();
       if (_waitingQueue.isNotEmpty) {
-        _ongoingQueue.add(_waitingQueue.removeFirst());
+        final execute = _waitingQueue.removeFirst();
+        _ongoingQueue.add(execute..call());
       }
       print(_ongoingQueue.length);
-    });
-  }
+    }
 
-  static void _fetch(Future? future) {
-    while (_waitingQueue.contains(future)) {}
+    void execute() {
+      final then = completer.complete;
+      final Function onError = completer.completeError;
+      super.fetch<T>(requestOptions).then(then).catchError(onError).whenComplete(next);
+    }
+
+    // 当正在进行的请求达到上限，则把新进来的请求放在等待队列中，等待进行中队列请求完成
+    if (_ongoingQueue.length < _capacity && _waitingQueue.isEmpty) {
+      _ongoingQueue.add(execute..call());
+    } else {
+      _waitingQueue.add(execute);
+    }
+    return completer.future;
   }
 }
